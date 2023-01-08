efdf6764c0a4846491678703f7a0721fb67e0983
Philip Gilde <44037700+phgilde@users.noreply.github.com>
2023-01-08 21:14:20 +0000

    spiel

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,16 @@
+import java.util.LinkedList;
+
+import item.Item;
+import room.Room;
 import view.View;
 
 class Game {
-    static View view;
+    static View currentView;
+    static Room currentRoom;
+    static LinkedList<Item> inventory;
+    
     public static void main(String[] args) {
-        System.out.println(view.getDescription());
+        System.out.println("Willkommen bei Murder House!");
     }
     
 }
