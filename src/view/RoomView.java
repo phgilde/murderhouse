a9cec8dd5e46fcff4cb4503fb33f625936a352c9
phgilde <ph.gilde@gmail.com>
2023-01-08 22:41:58 +0100

    raumansicht

---
+++ src/view/RoomView.java
@@ -0,0 +1,17 @@
+package view;
+
+public class RoomView extends View {
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String interact(String action) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
