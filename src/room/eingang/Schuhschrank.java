8487ba4ac7faa14bc01513ee3bfecc934af97361
Philip Gilde <44037700+phgilde@users.noreply.github.com>
2023-01-17 21:08:08 +0000

    formatierung

--- src/room/eingang/Schuhschrank.java
+++ src/room/eingang/Schuhschrank.java
@@ -6,31 +6,32 @@ import java.util.Optional;
 import item.Item;
 import item.Schuhe;
 
-public class Schuhschrank extends View{
+public class Schuhschrank extends View {
     private HashMap<String, Item> items = new HashMap<>();
 
-    public Schuhschrank(){
-     items.put("Schuhrosa", new Schuhe("rosa"));
-     items.put("Schuhbraun", new Schuhe("braun"));
-     items.put("Schuhblau", new Schuhe("blau"));
-     items.put("Schuhschwarz", new Schuhe("schwarz"));
-     items.put("Schuhweiß", new Schuhe("weiß"));
-     items.put("Schuhgrau", new Schuhe("grau"));
-     
+    public Schuhschrank() {
+        items.put("Schuhrosa", new Schuhe("rosa"));
+        items.put("Schuhbraun", new Schuhe("braun"));
+        items.put("Schuhblau", new Schuhe("blau"));
+        items.put("Schuhschwarz", new Schuhe("schwarz"));
+        items.put("Schuhweiß", new Schuhe("weiß"));
+        items.put("Schuhgrau", new Schuhe("grau"));
+
     }
 
-    public String getDescription(){
-      return "du siehst sechs Schuhe"; 
+    public String getDescription() {
+        return "du siehst sechs Schuhe";
     }
+
     @Override
-    public String interact(Optional<Item> heldItem){
-        return null; 
+    public String interact(Optional<Item> heldItem) {
+        return null;
 
     }
 
     @Override
     public Map<String, Item> getAvailableItems() {
-        
+
         return items;
     }
 }
