48a2af17d99729e75ecbe5e3f5a4357f3788ecfd
Liennek <122086090+Liennek@users.noreply.github.com>
2023-01-17 13:10:51 +0000

    Pflanze

---
+++ src/room/eingang/Pflanze.java
@@ -0,0 +1,20 @@
+import java.util.Optional;
+
+import item.Item;
+import room.view.View;
+
+public class Pflanze extends View {
+
+    @Override
+    public String getDescription() {
+        
+        return "Das ist eine GrOßE gRüNe ZiMmErPlAnZe";
+    }
+
+    @Override
+    public Object interact(Optional<Item> heldItem) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
