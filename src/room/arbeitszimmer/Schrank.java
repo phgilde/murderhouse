572066eff8c01f8b9a00130ddbe812de6b1df942
Anton1501 <67712068+Anton1501@users.noreply.github.com>
2023-01-27 09:47:10 +0000

    description schrank

--- src/room/arbeitszimmer/Schrank.java
+++ src/room/arbeitszimmer/Schrank.java
@@ -9,14 +9,14 @@ public class Schrank extends View {
 
     @Override
     public String getDescription() {
-        // TODO Auto-generated method stub
-        return null;
+        return "Du suchst nach dem nächsten Meisterwerk, das du lesen willst, als plötzlich etwas glänzendes deine Aufmerksamkeit weckt.\n"
+        + "Im *Schrank* ist ein grünes Schloss versteckt. Scheinbar wollte Ole etwas verbergen";
     }
 
     @Override
     public String interact(Optional<Item> heldItem) {
         // TODO Auto-generated method stub
-        return null;
+        return "";
     }
     
 }
