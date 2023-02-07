76b45a2d6038349bf2944771ac6c07e50a44992d
Anton1501 <67712068+Anton1501@users.noreply.github.com>
2023-02-07 13:06:06 +0000

    Interagiere  Schrank

--- src/room/arbeitszimmer/Schrank.java
+++ src/room/arbeitszimmer/Schrank.java
@@ -8,15 +8,28 @@ import room.view.View;
 public class Schrank extends View {
 
     @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return "schrank";
+    }
+    
+    @Override
     public String getDescription() {
         return "Du suchst nach dem nächsten Meisterwerk, das du lesen willst, als plötzlich etwas glänzendes deine Aufmerksamkeit weckt.\n"
-        + "Im *Schrank* ist ein grünes Schloss versteckt. Scheinbar wollte Ole etwas verbergen";
+        + "Im *Schrank* ist ein grünes Schloss versteckt. Scheinbar wollte Ole etwas verbergen...";
     }
 
     @Override
     public String interact(Optional<Item> heldItem) {
-        // TODO Auto-generated method stub
-        return "";
+        if(heldItem.isPresent()){
+            if(heldItem.get().getName().equals("gruener schluessel")){
+                return "Der Schrant beginnt sich plötzlich zu bewegen und du schreckst zurück. *Es öffnet sich eine Tür zu einem Geheimzimmer*, dass du nun betreten kannst.";
+            }
+
+        }
+        return "Das hat nicht geklappt.";
     }
+
+    
     
 }
