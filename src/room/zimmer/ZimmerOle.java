b8578341369354b318bea1724430948ea32b19af
Liennek <122086090+Liennek@users.noreply.github.com>
2023-02-07 13:06:20 +0000

    ana and ask

--- src/room/zimmer/ZimmerOle.java
+++ src/room/zimmer/ZimmerOle.java
@@ -21,7 +21,8 @@ public class ZimmerOle extends Room {
     public String getDescription() {
         return "Ein helles Zimmer mit einem Doppelbett, einer Kommode und zwei Nachttischen. An der Wand hängt ein Bild von Oles Familie.\n"
                 + "Es erinnert dich an das Schlafzimer deiner Eltern."
-                + "Von hier aus kannst du in den *Flur* gehen.";
+                + "Von hier aus kannst du in den *Flur* gehen.\n"
+                + "Eine etwas ältere Frau sitz aufrecht im Bett";
     }
 
 
