b80e3351e52ebad3eaf3156857e6cc29fe3e9196
Anton1501 <67712068+Anton1501@users.noreply.github.com>
2023-02-07 13:06:31 +0000

    geheimzimmer works

--- src/room/arbeitszimmer/Arbeitszimmer.java
+++ src/room/arbeitszimmer/Arbeitszimmer.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import item.Item;
 import room.Room;
+import room.geheimzimmer.Geheimzimmer;
 import room.view.View;
 
 public class Arbeitszimmer extends Room{
@@ -37,8 +38,12 @@ public class Arbeitszimmer extends Room{
 
     @Override
     public void interact(View view, Optional<Item> heldItem) {
-        
-        
+        if(heldItem.isPresent()){
+            if(view.getName().equals("schrank") && heldItem.get().getName().equals("gruener schluessel") && !adjacentRooms.contains("geheimzimmer")){
+                adjacentRooms.add("geheimzimmer");
+                System.out.println("raumoffen");
+            }
+        }
     }
     
 }
