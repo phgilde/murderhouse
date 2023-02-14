ec51e22a1b980b135bdc6a0bbee8c7d86d24e2a2
Philip Gilde <44037700+phgilde@users.noreply.github.com>
2023-02-14 13:00:43 +0000

    fleisch im kühlschrank

--- src/room/kueche/Kueche.java
+++ src/room/kueche/Kueche.java
@@ -9,6 +9,7 @@ public class Kueche extends Room {
     public Kueche() {
         adjacentRooms.add("saal");
         views.put("herdplatte", new Herdplatte());
+        views.put("kuehlschrank", new Kuehlschrank());
     }
 
     @Override
@@ -19,6 +20,7 @@ public class Kueche extends Room {
     @Override
     public String getDescription() {
         return "Du befindest dich in der Kueche. Auf dem Tisch steht ein phantastisch duftender Schokokuchen. "
-                + "Hier befindet sich eine *Herdplatte* mit einem Kochtopf darauf. von hier kannst du in den *Saal* gehen.";
+                + "Hier befindet sich eine *Herdplatte* mit einem Kochtopf darauf. Neben der Herdplatte steht ein großer *Kuehlschrank*."
+                + "von hier kannst du in den *Saal* gehen.";
     }
 }
