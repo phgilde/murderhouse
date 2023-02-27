80c1fe97e2a2276e94f5a8f90794938028f4fab0
phgilde <ph.gilde@gmail.com>
2023-02-27 23:56:47 +0100

    details

--- src/room/draussen/Draussen.java
+++ src/room/draussen/Draussen.java
@@ -11,10 +11,10 @@ public class Draussen extends Room {
 
     @Override
     public String getDescription() {
-        return "Du stehst vor dem Eingang des Hauses. Das Haus steht inmitten einer kleinen Insel. Um dich herum ist nur Wasser. "
+        return "Du stehst vor dem Eingang des Hauses. Das Haus steht auf einer kleinen Insel. Um dich herum ist nur Wasser. "
                 + "In der Ferne siehst du Festland, und kleine Boote, die sich auf die Insel zu bewegen. "
-                + "Vor dir ist eine Klippe zum Wasser hin. Eine in den Stein gehauene Treppe führt zu einem kleinen Strand."
-                + "Vor hier kannst du wieder in den *Eingang* gehen.";
+                + "Vor dir ist eine Klippe zum Wasser hin. Ein gepflasterer Pfad führt zu einer in den Stein gehauenen"
+                + " Treppe führt zu einem Steg, seltsamerweise siehst du an diesem aber kein Boot."
+                + "Vor hier kannst du wieder in den *Eingang* gehen. Ein Weg durch das Gestrüpp führt in den *Garten*.";
     }
-
 }
