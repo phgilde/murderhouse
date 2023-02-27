5dbdabdd6474d7ee982a089fcfab0ac0e5638dfe
phgilde <ph.gilde@gmail.com>
2023-02-27 22:09:27 +0100

    bilderrahmen

--- src/room/eingang/Eingang.java
+++ src/room/eingang/Eingang.java
@@ -5,14 +5,15 @@ import room.Room;
 import room.view.View;
 
 public class Eingang extends Room {
-   
+
 
     public Eingang() {
         adjacentRooms.add("saal");
         adjacentRooms.add("draussen");
         views.put("schluesselkiste", new Schluesselkiste());
         views.put("schuhschrank", new Schuhschrank());
-        views.put("planze", new Pflanze()); 
+        views.put("planze", new Pflanze());
+        views.put("bilderrahmen", new Bilderrahmen());
     }
 
     public String getName() {
@@ -21,7 +22,13 @@ public class Eingang extends Room {
 
     @Override
     public String getDescription() {
-        return "In dem *Eingang* befindet sich ein *Schuhschrank*, eine *Pflanze*, eine *Schluesselbox* und die Eingangstuer";
+        return "Du stehst im Eingangsbereich des Hauses. Du merkst dem Raum an, dass seine Einrichter sich der Wichtigkeit des ersten Eindruckes bewusst sind."
+                + "Der Boden ist mit einem stilvollen Teppich ausgelegt, der die Eingangstuer umrahmt."
+                + "Die Wande sind mit einem edlen Tapetenbehang verziert."
+                + "Die Decke ist mit einem prachtvollen Kronleuchter beleuchtet."
+                + "Was du zunächst für einen Picasso-Druck hältst, ist in Wirklichkeit ein goldverzierter Spiegel, der den Raum optisch vergrößert."
+                + "Gegenüber des Spiegels hängt ein *Bilderrahmen*, dessen Gemaelde du nicht genau erkennen kannst."
+                + "Hier befindet sich ein *Schuhschrank*, eine *Pflanze*, eine *Schluesselbox* und die Eingangstuer";
 
     }
 
