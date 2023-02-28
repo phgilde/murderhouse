e99ec3f1de677b58478029d32208467e234fe038
Anton1501 <67712068+Anton1501@users.noreply.github.com>
2023-02-28 19:57:28 +0000

    dead in game implemented

--- src/murderhouse/room/view/human/Human.java
+++ src/murderhouse/room/view/human/Human.java
@@ -5,7 +5,6 @@ import murderhouse.item.Item;
 import murderhouse.room.view.View;
 
 public abstract class Human extends View {
-     protected int n = 0; 
     /**
      * Gibt eine Reaktion auf ein Item zurueck.
      * 
@@ -26,14 +25,4 @@ public abstract class Human extends View {
      * 
      * 
      */
-    public String totallyNotDead (int t){
-        if(t==1){
-          return this.getName() + "ist nicht tot, diese Person wird nur ab jetzt komischer Weise nicht mehr ansprechbar sein und nur noch auf dem Boden rumliegen."; 
-        }else{
-            return ""; 
-        }
-    }
-
-
-
 }
