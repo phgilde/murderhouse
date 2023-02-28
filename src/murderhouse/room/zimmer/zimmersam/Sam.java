63e24cae27c57dd60a67e0cbb79be3e7a7426137
phgilde <ph.gilde@gmail.com>
2023-02-28 20:55:50 +0100

    n == 0 entfernt

--- src/murderhouse/room/zimmer/zimmersam/Sam.java
+++ src/murderhouse/room/zimmer/zimmersam/Sam.java
@@ -9,31 +9,27 @@ public class Sam extends Human {
 
     @Override
     public String talk(Optional<Item> heldItem) {
-            if (heldItem.isEmpty()) {
-                return "'Geh weg!'";
-            } else if (heldItem.get().getName().equals("boxhandschuhe")) {
-                return "'Ich boxe in meiner Freizeit. Ich mache manchmal bei Boxkaempfen mit die ... naja die nur so quasi legal sind'";
+        if (heldItem.isEmpty()) {
+            return "'Geh weg!'";
+        } else if (heldItem.get().getName().equals("boxhandschuhe")) {
+            return "'Ich boxe in meiner Freizeit. Ich mache manchmal bei Boxkaempfen mit die ... naja die nur so quasi legal sind'";
 
-            } else {
-                return "'Ich will nicht mit dir reden'";
-            }
+        } else {
+            return "'Ich will nicht mit dir reden'";
+        }
     }
 
     @Override
     public String getDescription() {
-            return "Sam ist etwa 18-Jahre alt, er hat eine sportliche Figur.";
+        return "Sam ist etwa 18-Jahre alt, er hat eine sportliche Figur.";
     }
 
     @Override
     public String ask(String q) {
-        if (n == 0) {
-            if (q == "knall") {
-                return "'Knall, welcher Knall? Papa hat nur die Kuehlschranktuer zugeworfen, sonst war da nichts'";
-            } else if (q == "streit") {
-                return "'Er nervt -- Hat immer rumgenervt wegen meinen Verhaftungen, er glaubt das ich mein Leben nicht auf die Reihe kriege.'";
-            } else {
-                return null;
-            }
+        if (q == "knall") {
+            return "'Knall, welcher Knall? Papa hat nur die Kuehlschranktuer zugeworfen, sonst war da nichts'";
+        } else if (q == "streit") {
+            return "'Er nervt -- Hat immer rumgenervt wegen meinen Verhaftungen, er glaubt das ich mein Leben nicht auf die Reihe kriege.'";
         } else {
             return null;
         }
--- src/murderhouse/room/zimmer/zimmertrude/Trude.java
+++ src/murderhouse/room/zimmer/zimmertrude/Trude.java
@@ -9,39 +9,29 @@ public class Trude extends Human {
 
     @Override
     public String talk(Optional<Item> heldItem) {
-        if (n == 0) {
-            if (heldItem.isEmpty()) {
-                return "'Er ist tot! Wie konnte das nur passieren???'";
-            } else if (heldItem.get().getName().equals("baseballschlaeger")) {
-                return "'Ja das ist Ben's. Oh was ist den da soviel Farbe dran? Da habe ich wohl beim Malen nicht gut aufgepasst.'";
+        if (heldItem.isEmpty()) {
+            return "'Er ist tot! Wie konnte das nur passieren???'";
+        } else if (heldItem.get().getName().equals("baseballschlaeger")) {
+            return "'Ja das ist Ben's. Oh was ist den da soviel Farbe dran? Da habe ich wohl beim Malen nicht gut aufgepasst.'";
 
-            } else {
-                return "'Darueber weiß ich leider nichts.'";
-            }
         } else {
-            return null;
+            return "'Darueber weiß ich leider nichts.'";
         }
+
     }
 
     @Override
     public String getDescription() {
-        if (n == 0) {
-            return "Trude ist eine etwa mitte 20-Jahre alte Frau.";
-        } else {
-            return "Trude scheint zu schlafen.";
-        }
+        return "Trude ist eine etwa mitte 20-Jahre alte Frau.";
+
     }
 
     @Override
     public String ask(String q) {
-        if (n == 0) {
-            if (q == "malsachen") {
-                return "'Ich male gerne um zu entspannen'";
-            } else if (q == "streit") {
-                return "'Ja wir streiten uns manchmal wegen der Firma. Aber ich würde ihm nie etwas tun! Er ist mein Vater!'";
-            } else {
-                return null;
-            }
+        if (q == "malsachen") {
+            return "'Ich male gerne um zu entspannen'";
+        } else if (q == "streit") {
+            return "'Ja wir streiten uns manchmal wegen der Firma. Aber ich würde ihm nie etwas tun! Er ist mein Vater!'";
         } else {
             return null;
         }
